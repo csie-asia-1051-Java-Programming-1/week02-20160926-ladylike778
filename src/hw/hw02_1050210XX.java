3c305dd751343610974c607e94898b739de38cf0
ladylike778 <105021037@live.asia.edu.tw>
2016-09-30 12:09:37 +0800

    hw finished

--- src/hw/hw02_1050210XX.java
+++ src/hw/hw02_1050210XX.java
@@ -1,10 +1,29 @@
 package hw;
-
+/*
+ * Topic: 已知男生標準體重＝(身高－80 )*0.7；女生標準體重＝(身高－70)*0.6；
+ * 試寫一個程式可以計算男生女生的標準體重。
+ * (輸入兩個數值，依序代表為身高及性別
+ * （1代表男性；2代表女性）)
+ * Date: 2016/09/26
+ * Author: 105021037 林建宏
+ */
+import java.util.Scanner;
 public class hw02_1050210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入身高");
+		int tall =scn.nextInt();
+		System.out.println("請輸入性別:1為男生2為女生");
+		int g= scn.nextInt();
+		float w=0;
+		if (g == 1){ w=(tall-80)*0.7f; 
+			
+		}else if (g==2){w=(tall-70)*0.6f;
+		
+		}
+		System.out.println("標準體重為"+w);
 	}
 
 }
--- src/hw/hw03_1050210XX.java
+++ src/hw/hw03_1050210XX.java
@@ -1,10 +1,30 @@
 package hw;
-
+/*
+ * Topic: 試撰寫一個程式，可由鍵盤讀入一個 4 位數的整數，代表西洋的年份，然後判別這個年份是否為閏年
+ * （每四年一閏，每百年不閏，每四百年一閏，例如西元 1900 雖為 4 的倍數，但可被 100 整除，
+ * 所以不是閏年，同理， 2000 年是閏年，因可被 400 整數，而 2004 當然也是閏年，因可以被 4 整除）。
+ * Date: 2016/09/26
+ * Author: 105021037 林建宏
+ */
+import java.util.Scanner;
 public class hw03_1050210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入西元年分");
+		int Y= scn.nextInt();
+		int a=0;
+		if(Y%400==0 && Y%4==0){
+			System.out.print("閏年");
+		}else{
+			System.out.print("非閏年");
+		}
+		
+		}
 	}
+	
 
-}
+	
+
+
